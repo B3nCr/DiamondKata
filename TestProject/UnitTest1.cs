f5e86ec3f838d4f61fb8e6fc9a33ee12003374f6
Ben Crinion <ben.crinion@sorted.com>
2020-04-12 20:41:17 +0100

    Failing test for duplicate character.

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -25,7 +25,7 @@ namespace TestProject
         {
             var result = _diamondBuilder.Build('B');
             Assert.Equal(" A ", result[0]);
-            Assert.Equal("B", result[1]);
+            Assert.Equal("BB", result[1]);
         }
 
         [Fact]
@@ -33,7 +33,7 @@ namespace TestProject
         {
             var result = _diamondBuilder.Build('B');
             Assert.Equal(" A ", result[0]);
-            Assert.Equal("B", result[1]);
+            Assert.Equal("BB", result[1]);
             Assert.Equal(" A ", result[2]);
         }
 
@@ -42,33 +42,20 @@ namespace TestProject
         {
             var result = _diamondBuilder.Build('C');
             Assert.Equal("  A  ", result[0]);
-            Assert.Equal(" B ", result[1]);
-            Assert.Equal("C", result[2]);
-            Assert.Equal(" B ", result[3]);
+            Assert.Equal(" BB ", result[1]);
+            Assert.Equal("CC", result[2]);
+            Assert.Equal(" BB ", result[3]);
             Assert.Equal("  A  ", result[4]);
         }
 
-        [Fact(Skip = "true")]
+        [Fact]
         public void Build_B_ReturnsExpected()
         {
             var result = _diamondBuilder.Build('B');
             Assert.Equal(3, result.Length);
             Assert.Equal(" A ", result[0]);
-            Assert.Equal("B B", result[1]);
+            Assert.Equal("BB", result[1]);
             Assert.Equal(" A ", result[2]);
         }
-
-        [Fact(Skip = "true")]
-        public void Build_C_ReturnsExpected()
-        {
-            var result = _diamondBuilder.Build('C');
-            Assert.Equal(result.Length, 5);
-            Assert.Equal("  A  ", result[0]);
-            Assert.Equal(" B B ", result[1]);
-            Assert.Equal("C   C", result[2]);
-            Assert.Equal(" B B ", result[3]);
-            Assert.Equal("  A  ", result[4]);
-        }
     }
-
 }
